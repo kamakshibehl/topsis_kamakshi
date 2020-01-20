f8a6bbb703bdc59a2035feaff0d8ee629d8d2fc4
kamakshibehl <43804730+kamakshibehl@users.noreply.github.com>
2020-01-20 22:24:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="topsis_kamakshi", # Replace with your own username
     version="0.0.1",
-    author="Kamakshi/kamakshi_behl",
+    author="kamakshi/kamakshi_behl",
     author_email="kamakshi.behl22@gmail.com",
     description="Implementation of topsis over a csv datafile using the distance calculation and comparison approach",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    
+    url="https://github.com/kamakshibehl/topsis_kamakshi.git",
+    download_url="https://github.com/kamakshibehl/topsis_kamakshi/archive/0.0.1.tar.gz"
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
