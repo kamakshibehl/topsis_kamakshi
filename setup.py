3a43fb5650f9aaef10a84f2ded4e6615dd2973d3
kamakshibehl <43804730+kamakshibehl@users.noreply.github.com>
2020-01-20 22:11:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 19:49:25 2020
+@author: kamakshi_behl
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topsis_kamakshi", # Replace with your own username
+    version="0.0.1",
+    author="Kamakshi/kamakshi_behl",
+    author_email="kamakshi.behl22@gmail.com",
+    description="Implementation of topsis over a csv datafile using the distance calculation and comparison approach",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
